fc2d26fa66f52304b334644b7c3f12291b54b746
Jonathan Parks <jqp@case.edu>
2016-02-24 11:36:31 -0500

    first commit

---
+++ lab1/inverter output waveform.tcl
@@ -0,0 +1,28 @@
+############################################################
+## EZwave - Saved Window File
+## Sunday, February 21, 2016 at 5:55:35 PM EST
+##
+## Note: This is an auto-generated file.
+##
+## In case of modification, Do not remove this comment
+############################################################
+
+# ===== Open required Database =====
+dataset open /tmp/inverter/ami05a/inverter_ami05a_01.wdb inverter_ami05a_01
+
+# ===== Open the window =====
+wave addwindow  -divider 0.90
+
+# ===== Create row #1 =====
+add wave  -show TRAN.v -color -16711936 INPUT
+
+
+
+
+# ===== Create row #2 =====
+add wave  -show TRAN.v -color -256 OUTPUT
+
+
+
+
+# ====== Create the cursors, markers and measurements =====
